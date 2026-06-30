29f9e63b052936a2d7dfc60dacdfe67aae283c66
Nishant Singh <nishant.singh2k06@gmail.com>
2026-06-30 21:49:57 +0530

    Time: 18 ms (47.45%), Space: 49.9 MB (90.65%) - LeetHub

---
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.java
@@ -0,0 +1,29 @@
+class BSTIterator {
+    private List<Integer> list;
+    private int idx;
+
+    public BSTIterator(TreeNode root) {
+        list = new ArrayList<>();
+        idx =0;
+        inorder(root);
+    }
+    private void inorder(TreeNode node){
+        if(node == null) return;
+        inorder(node.left);
+        list.add(node.val);
+        inorder(node.right);
+    }
+    
+    public int next() {
+        return list.get(idx++);
+    }
+    
+    public boolean hasNext() {
+        return idx<list.size();
+    }
+}
+class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode(int x) { val = x; }
+}
