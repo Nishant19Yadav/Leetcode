ca50bb3c90b7b6d156525007009462dd934b2868
Nishant Singh <nishant.singh2k06@gmail.com>
2026-07-15 22:44:44 +0530

    Time: 0 ms (100%), Space: 42.8 MB (56.64%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -3,17 +3,14 @@ class Solution {
         int n = nums.length;
         if(n==1) return nums[0];
 
-        int[] dp = new int[n];
-
-        dp[0]=nums[0];
-        dp[1] = Math.max(nums[0],nums[1]);
+        int prev2=nums[0];
+        int prev1 = Math.max(nums[0],nums[1]);
 
         for(int i=2;i<n;i++){
-            int p = nums[i]+dp[i-2];
-            int np = dp[i-1];
+            int curr = Math.max(prev1,nums[i]+prev2);
+            prev2 = prev1;prev1 = curr;
 
-            dp[i] = Math.max(p,np);
         }
-        return dp[n-1];
+        return prev1;
     }
 }
