d26ac6635a67b18d87dad86e91085b0e7e684b50
Nishant Singh <nishant.singh2k06@gmail.com>
2026-07-16 22:03:42 +0530

    Time: 2 ms (99.8%), Space: 47.1 MB (23.37%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -1,32 +1,24 @@
 class Solution {
-    int[] points;
-    int[] dp;
     public int deleteAndEarn(int[] nums) {
         int max=0;
         for(int num:nums){
-            max=Math.max(max,num);
+            max =Math.max(max,num);
         }
-        points = new int[max+1];
-        for(int num:nums){
-            points[num]+=num;
+        int[] points = new int[max+1];
+        for (int num : nums)
+            points[num] += num;
+        if(max==0) return 0;
+
+        int[] dp = new int[max+1];
+
+        dp[0]=points[0];
+        if(max>=1){
+            dp[1] = Math.max(points[0],points[1]);
         }
-        dp = new int[max+1];
-        Arrays.fill(dp,-1);
-        return solve(max);
 
-
+        for(int i=2;i<=max;i++){
+            dp[i] = Math.max(dp[i-1],points[i]+dp[i-2]);
+        }
+        return dp[max];
     }
-    private int solve(int i){
-        if(i<=0){
-            return 0;
-        };
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        int take = points[i]+solve(i-2);
-        int skip = solve(i-1);
-
-        return dp[i]=Math.max(take,skip);
-    }
-
 }
