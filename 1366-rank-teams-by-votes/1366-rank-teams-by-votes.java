807092849cf39ef71f667caa2bb1c3bf586d6cd0
Nishant Singh <nishant.singh2k06@gmail.com>
2026-07-17 21:29:53 +0530

    Time: 9 ms (60.91%), Space: 45.5 MB (57.61%) - LeetHub

--- 1366-rank-teams-by-votes/1366-rank-teams-by-votes.java
+++ 1366-rank-teams-by-votes/1366-rank-teams-by-votes.java
@@ -1,31 +1,33 @@
 class Solution {
     public String rankTeams(String[] votes) {
-        int n = votes[0].length();
-        int[][] cnt = new int[26][26];
+        Map<Character,int[]> map = new HashMap<>();
 
+        int n = votes[0].length();
+        for(char c:votes[0].toCharArray()){
+            map.put(c,new int[n]);
+        }
         for(String vote:votes){
-            for(int pos=0;pos<n;pos++){
-                char team = vote.charAt(pos);
-                cnt[team-'A'][pos]++;
+            for(int i=0;i<n;i++){
+                map.get(vote.charAt(i))[i]++;
             }
         }
-        Character[] teams = new Character[n];
-        for (int i = 0; i < n; i++) {
-            teams[i] = votes[0].charAt(i);
-        }
-
-        Arrays.sort(teams,(a,b)->{
-            for(int pos=0;pos<n;pos++){
-                if(cnt[a-'A'][pos]!=cnt[b-'A'][pos]){
-                    return cnt[b-'A'][pos]-cnt[a-'A'][pos];
+        List<Character> teams = new ArrayList<>(map.keySet());
+        Collections.sort(teams,(a,b)->{
+            int[] x = map.get(a);
+            int[] y = map.get(b);
+            for(int i=0;i<n;i++){
+                if(x[i]!=y[i]){
+                    return y[i]-x[i];
                 }
+                
             }
             return a-b;
         });
-        StringBuilder sb = new StringBuilder();
-        for(char c:teams){
-            sb.append(c);
-        }
-        return sb.toString();
+    StringBuilder sb = new StringBuilder();
+    for(char c:teams){
+        sb.append(c);
     }
+    return sb.toString();
+    }
+
 }
