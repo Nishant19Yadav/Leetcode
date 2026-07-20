d19af42b77c02d04afa56f76411326e19a45d502
Nishant Singh <nishant.singh2k06@gmail.com>
2026-07-20 19:42:12 +0530

    Time: 6 ms (55.4%), Space: 47.3 MB (44.14%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -1,30 +1,55 @@
 class Solution {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
-        int n = grid.length;
-        int m = grid[0].length;
 
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int[] row:grid){
-            for(int num:row){
-                list.add(num);
-            }
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        int n = rows * cols;
+
+        k %= n;
+
+        if (k == 0) {
+            return build(grid);
         }
-        int size = list.size();
-        k%=size;
-        Collections.rotate(list,k);
+
+        reverse(grid, 0, n - 1, cols);
+        reverse(grid, 0, k - 1, cols);
+        reverse(grid, k, n - 1, cols);
+
+        return build(grid);
+    }
+
+    private void reverse(int[][] grid, int left, int right, int cols) {
+
+        while (left < right) {
+
+            int r1 = left / cols;
+            int c1 = left % cols;
+
+            int r2 = right / cols;
+            int c2 = right % cols;
+
+            int temp = grid[r1][c1];
+            grid[r1][c1] = grid[r2][c2];
+            grid[r2][c2] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    private List<List<Integer>> build(int[][] grid) {
 
         List<List<Integer>> ans = new ArrayList<>();
-        int idx=0;
-        for(int i=0;i<n;i++){
-            List<Integer> row = new ArrayList<>();
-            for(int j=0;j<m;j++){
-                row.add(list.get(idx++));
 
+        for (int[] row : grid) {
+            List<Integer> list = new ArrayList<>();
+            for (int num : row) {
+                list.add(num);
             }
-            ans.add(row);
+            ans.add(list);
         }
+
         return ans;
-
-
     }
 }
