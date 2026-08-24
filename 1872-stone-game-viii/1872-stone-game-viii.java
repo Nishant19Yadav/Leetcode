c75ee44b2170b8d8019b82d4c4eb92e3040948e9
Nishant Singh <nishant.singh2k06@gmail.com>
2026-08-24 21:21:01 +0530

    Time: 3 ms (100%), Space: 82.8 MB (99.15%) - LeetHub

---
+++ 1872-stone-game-viii/1872-stone-game-viii.java
@@ -0,0 +1,16 @@
+class Solution { 
+    public int stoneGameVIII(int[] stones) { 
+        int n = stones.length; 
+        for (int i = 1; i < n; i++) { 
+            stones[i] += stones[i - 1];  // calculating the prefix sum
+        } 
+ 
+        int best = stones[n - 1];  // case when i will remove all stons from the array
+ 
+        for (int i = n - 2; i >= 1; i--) { 
+            best = Math.max(best,stones[i] - best);  //currentprefix sum- opponent best advantage
+        } 
+ 
+        return best; 
+    } 
+}    
